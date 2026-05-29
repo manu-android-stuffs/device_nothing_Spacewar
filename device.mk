e5d3f807448b2dbb4e35af7fb555440c5e8f117c
manugame <braibant.manu@gmail.com>
2026-05-29 19:14:28 +0200

    Spacewar: overwrite ota server

--- device.mk
+++ device.mk
@@ -33,7 +33,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    NcmTetheringOverlay
+    NcmTetheringOverlay \
+    OverwriteUpdaterUrl
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
